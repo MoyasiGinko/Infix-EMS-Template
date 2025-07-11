05af7fc63b5b914ec2aed239c8c6a15fe8e013dd
moyasiginko <mrsiyam61@gmail.com>
2025-07-12 01:22:36 +0600

    Update class_routine_print.blade.php

--- resources/views/backEnd/academics/class_routine_print.blade.php
+++ resources/views/backEnd/academics/class_routine_print.blade.php
@@ -55,7 +55,7 @@
                         class="text-white mb-0">
                         {{ isset(generalSetting()->school_name) ? generalSetting()->school_name : 'Infix School Management ERP' }}
                     </h3>
-                    <span style="font-size:16px !important;margin-right:10px; text-align:right; display:block;"
+                    <span style="font-size:18px !important;margin-right:10px; text-align:right; display:block;"
                         class="text-white">{{ isset(generalSetting()->school_name_ar) ? generalSetting()->school_name_ar : 'مركز العلوم علي الإسلامية دكا' }}
                     </span>
                     <br>
