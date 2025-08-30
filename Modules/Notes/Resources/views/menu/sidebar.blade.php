00af60e196d264e9966379d95cc49cb638196faf
moyasiginko <mrsiyam61@gmail.com>
2025-08-30 17:11:27 +0600

    Simplify Notes module - basic sidebar entry without permissions

--- Modules/Notes/Resources/views/menu/sidebar.blade.php
+++ Modules/Notes/Resources/views/menu/sidebar.blade.php
@@ -1,34 +1,14 @@
 {{-- Notes Module Start --}}
 @if(auth()->user()->role_id == 1)
     <li data-position="999" class="sortable_li">
-        <a href="javascript:void(0)" class="has-arrow" aria-expanded="false">
+        <a href="{{ route('notes.index') }}">
             <div class="nav_icon_small">
                 <span class="fas fa-sticky-note"></span>
             </div>
             <div class="nav_title">
-                <span>@lang('Notes')</span>
+                <span>Notes</span>
             </div>
         </a>
-        <ul class="list-unstyled" id="subMenuNotes">
-            <li data-position="1000">
-                <a href="{{ route('notes.expenses.index') }}">@lang('Expense Notes')</a>
-            </li>
-            <li data-position="1001">
-                <a href="{{ route('notes.incomes.index') }}">@lang('Income Notes')</a>
-            </li>
-            <li data-position="1002">
-                <a href="{{ route('notes.events.index') }}">@lang('Event Notes')</a>
-            </li>
-            <li data-position="1003">
-                <a href="{{ route('notes.incidents.index') }}">@lang('Incident Notes')</a>
-            </li>
-        </ul>
     </li>
-@endif
-{{-- Notes Module End --}}
-                @endif
-            </ul>
-        </li>
-    @endif
 @endif
 {{-- Notes Module End --}}
--- Modules/Notes/Routes/web.php
+++ Modules/Notes/Routes/web.php
@@ -11,23 +11,6 @@ Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'notes', 'as' => 'not
     Route::put('{note}', 'Modules\Notes\Http\Controllers\NoteController@update')->name('update');
     Route::delete('{note}', 'Modules\Notes\Http\Controllers\NoteController@destroy')->name('destroy');
 
-    // Category-specific routes
-    Route::group(['prefix' => 'expenses', 'as' => 'expenses.'], function () {
-        Route::get('/', 'Modules\Notes\Http\Controllers\NoteController@expenses')->name('index');
-    });
-
-    Route::group(['prefix' => 'incomes', 'as' => 'incomes.'], function () {
-        Route::get('/', 'Modules\Notes\Http\Controllers\NoteController@incomes')->name('index');
-    });
-
-    Route::group(['prefix' => 'events', 'as' => 'events.'], function () {
-        Route::get('/', 'Modules\Notes\Http\Controllers\NoteController@events')->name('index');
-    });
-
-    Route::group(['prefix' => 'incidents', 'as' => 'incidents.'], function () {
-        Route::get('/', 'Modules\Notes\Http\Controllers\NoteController@incidents')->name('index');
-    });
-
     // Export routes
     Route::get('export/excel', 'Modules\Notes\Http\Controllers\NoteController@exportExcel')->name('export.excel');
     Route::get('export/pdf', 'Modules\Notes\Http\Controllers\NoteController@exportPdf')->name('export.pdf');
