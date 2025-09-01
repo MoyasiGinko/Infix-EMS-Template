4bc81492ca4f1fd417eaf24b02950f1e9c6f959b
moyasiginko <mrsiyam61@gmail.com>
2025-09-02 05:46:38 +0600

    fix the header

--- resources/views/backEnd/accounts/add_expense.blade.php
+++ resources/views/backEnd/accounts/add_expense.blade.php
@@ -292,21 +292,27 @@ if (!empty(@$setting->currency_symbol)) {
 
       <div class="col-lg-8 col-xl-9">
         <div class="white-box">
-          <div class="row">
-            <div class="col-lg-4 no-gutters">
+          <div class="row align-items-center mb-3">
+            <div class="col-6">
               <div class="main-title">
-                <h3 class="mb-15">@lang('accounts.expense_list') </h3>
+                <h3 class="mb-0">@lang('accounts.expense_list')</h3>
               </div>
             </div>
-            <div class="d-flex justify-content-end mb-3 flex-wrap">
-              <button type="button" class="primary-btn small fix-gr-bg mr-2 mb-2"
-                id="expExportExcel">@lang('common.export') XLSX</button>
-              <button type="button" class="primary-btn small fix-gr-bg mr-2 mb-2"
-                id="expExportCSV">@lang('common.export') CSV</button>
-              <button type="button" class="primary-btn small fix-gr-bg mr-2 mb-2"
-                id="expExportPDF">@lang('common.export') PDF</button>
-              <button type="button" class="primary-btn small fix-gr-bg mb-2"
-                id="expExportPrint">@lang('common.print')</button>
+            <div class="col-6 text-right">
+              <div class="d-inline-flex flex-wrap justify-content-end">
+                <button type="button" class="primary-btn small fix-gr-bg mr-2 mb-2" id="expExportExcel">
+                  @lang('common.export') XLSX
+                </button>
+                <button type="button" class="primary-btn small fix-gr-bg mr-2 mb-2" id="expExportCSV">
+                  @lang('common.export') CSV
+                </button>
+                <button type="button" class="primary-btn small fix-gr-bg mr-2 mb-2" id="expExportPDF">
+                  @lang('common.export') PDF
+                </button>
+                <button type="button" class="primary-btn small fix-gr-bg mb-2" id="expExportPrint">
+                  @lang('common.print')
+                </button>
+              </div>
             </div>
           </div>
 
